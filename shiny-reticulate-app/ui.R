dc92c0904a7c25de73297696b29142721aa83717
Arijit2812 <arijit4006@gmail.com>
2023-07-10 19:36:39 +1200

    All files updated
    All files updated from local

---
+++ shiny-reticulate-app/ui.R
@@ -0,0 +1,43 @@
+# Import R packages needed for the UI
+library(shiny)
+library(shinycssloaders)
+library(DT)
+
+ui <- fluidPage(
+  
+  titlePanel('Hoof conformation'),
+  
+  sidebarLayout(
+    
+    # ---------------- Sidebar panel with changeable inputs ----------------- #
+    sidebarPanel(
+      
+      h4('Select Hoof aspect'),
+      radioButtons(inputId = "aspect", label = "Hoof aspect", 
+                  choices = c("Shape", "Splay", "Growth","Heel", "Fetlock"), selected = "Shape"),
+      h4('Select a file'),
+      fileInput("myFile", "Choose an image file",
+                multiple = TRUE,
+                accept = c("image/png",
+                           "image/jpeg",
+                           ".JPG")),
+      actionButton(inputId = "Go", label = "Get Prediction", icon = icon("play"))
+#      br(),
+#      sliderInput('n',
+#                  'Number of pics:',
+#                  value = 1,
+#                  min = 1,
+#                  max = 10),
+#      hr()
+    ),
+    
+    # ---------------- Sidebar panel with changeable inputs ----------------- #
+    mainPanel(
+      
+      # Output: Image output ----
+      plotOutput('myImage'),
+      br(),
+      uiOutput('myResult')
+    )
+  )
+)
